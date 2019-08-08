05158f519668e1409ea3ea348f45ae1226768583
Lolicanadian <50314143+Lolicanadian@users.noreply.github.com>
2019-08-08 13:07:29 -0600

    Add files via upload
    Repository test for pictures and menu features.

--- PVPSelect.java
+++ PVPSelect.java
@@ -1,9 +1,5 @@
 import java.awt.*;
 import javax.swing.JPanel;
-
-import javafx.scene.layout.Pane;
-import sun.applet.Main;
-
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -15,49 +11,49 @@ import javax.sound.sampled.AudioInputStream;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
-public class Menu extends JPanel {
+public class PVPSelect extends JPanel {
 	
-	//instance variables
 	boolean difficultyCPU = false;
 	boolean started = false;
 	boolean started2 = false;
+
 	
-	public Menu() {
-		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	public PVPSelect() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
-		// lowers position of the buttons
-		add(Box.createVerticalStrut(280));
-		
+		System.out.println("works");
 		
 		//draws button 1 and its properties.
-		 CustomButton button1 = new CustomButton("Player vs Player");
-		 button1.setAlignmentX(LEFT_ALIGNMENT);
-		 button1.setAlignmentY(CENTER_ALIGNMENT);
+		 CustomButton button1 = new CustomButton("EASY");
+		 button1.setAlignmentX(CENTER_ALIGNMENT);
+		 //button1.setAlignmentY(TOP_ALIGNMENT);
+		 button1.setBounds(40, 100, 100, 60);
 		 
 		 // button 1 pressed
 		 button1.addActionListener(new AbstractAction() {
 			 @Override
 			 public void actionPerformed(ActionEvent e) {
 				 PVP(); // this will take us to pvp. 
+				 
 			 }
 			 
 		 });
 		 
+
 		 
 		 
 		 
 		 //draws button 2 and its positions
-		 CustomButton button2 = new CustomButton("Player vs Computer");
-		 button2.setAlignmentX(RIGHT_ALIGNMENT);
-		 button2.setAlignmentY(CENTER_ALIGNMENT);
+		 setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		 CustomButton button2 = new CustomButton("MEDIUM");
+		 button2.setAlignmentX(CENTER_ALIGNMENT);
+		 //button2.setAlignmentY(CENTER_ALIGNMENT);
 		 
 		 // button 2 pressed
 		 button2.addActionListener(new AbstractAction() {
 			 @Override
 			 public void actionPerformed(ActionEvent e) {
-				
-				PVC(); //This will take us to pvc.
-				
+				PVE();
 				
 			 }
 			 
@@ -67,16 +63,17 @@ public class Menu extends JPanel {
 		 
 		 
 		 //draws button 3 and its positions
-		 CustomButton button3 = new CustomButton("EXIT");
-		 button3.setAlignmentX(LEFT_ALIGNMENT);
-		 button3.setAlignmentY(BOTTOM_ALIGNMENT);
+		 //setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		 CustomButton button3 = new CustomButton("HARD");
+		 button3.setAlignmentX(CENTER_ALIGNMENT);
+		 //button3.setAlignmentY(BOTTOM_ALIGNMENT);
 		 
 		 // button 3 pressed
 		 button3.addActionListener(new AbstractAction() {
 			 @Override
 			 public void actionPerformed(ActionEvent e) {
-				 
-				 System.exit(0); // We need the computer gui from adarsha.
+			
+				 PVP(); // We need the computer gui from adarsha.
 			 }
 			 
 		 });
@@ -90,25 +87,19 @@ public class Menu extends JPanel {
 			 
 		//add(Box.createVerticalGlue());
 		
-		try {
-			URL url = Menu.class.getResource("Poketheme.wav");
-			AudioInputStream audio = AudioSystem.getAudioInputStream(url);
-			Clip music = AudioSystem.getClip();
-			music.open(audio);
-			music.loop(-1);
-		} catch (Exception ex) {}	
-	}
+	}	 
 	
 	// Player vs Player 
 	public void PVP() {
 		removeAll();
-		repaint();
 		started = true;
+		repaint();
+		
 	}
 	
-	public void PVC() {
+	public void PVE() {
 		removeAll();
-		started2 = true; 
+		started2 = true;
 		repaint();
 		
 	}
@@ -119,22 +110,21 @@ public class Menu extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		if (started) {
-			//System.out.println("Hello!");
-			Main2.main(null);
+			g.drawImage(new ImageIcon(Menu.class.getResource("PokemonCard.png")).getImage(), 0, 0, 640, 480, this);
+			gui.main(null);
 			
-		
 		}
 		else if (started2) {
-			System.out.println("Player vs Computer");
+			System.out.println("Yipee!");
 		}
-		else {
-			System.out.println("");
-			/*
+		else
+		{
+			
 			g.drawImage(new ImageIcon(Menu.class.getResource("PokeMenu.png")).getImage(), 0, 0, 840, 840, this);
-			g.drawImage(new ImageIcon(Menu.class.getResource("PokemonCard.png")).getImage(), 300, 300, 240, 240, this);
-			g.drawImage(new ImageIcon(Menu.class.getResource("Pikachu.png")).getImage(), 550, 300, 240, 240, this);
-			*/
+			g.drawImage(new ImageIcon(Menu.class.getResource("Jigglypuff.png")).getImage(), 550, 300, 240, 240, this);
 		}
 	}
-
+	
+	
+	
 }
